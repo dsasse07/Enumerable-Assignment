ab81b4593b82c538235c444d5ccc2270c8d6ab36
Daniel Sasse <dsasse07@gmail.com>
2020-12-16 08:56:14 -0500

    Done

--- run.rb
+++ run.rb
@@ -29,6 +29,38 @@ def instructors
     ]
 end
 
+def all_names
+    instructors.map{|instructor| instructor[:name]}
+end
+
+def name_and_role
+    instructors.map{|instructor| {instructor[:name] => instructor[:role]}}
+end
+
+def all_coaches
+    instructors.select {|instructor| instructor[:role] == "Coach"}
+end
+
+def all_leads
+    instructors.select {|instructor| instructor[:role] == "Lead"}
+end
+
+def first_instructor_with_a_car
+    instructors.find {|instructor| instructor[:owns_car] == true}
+end
+
+def instructors_with_more_than_one_sibling
+    instructors.select {|instructor| instructor[:siblings] > 1}
+end
+
+def most_siblings
+    instructors.max_by {|instructor| instructor[:siblings]}
+end
+
+def total_siblings
+    instructors.sum {|instructor| instructor[:siblings]}
+end
+
 
 binding.pry
 0
